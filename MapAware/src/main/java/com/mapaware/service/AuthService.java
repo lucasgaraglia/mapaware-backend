0b1ec8a5ea413239caf466eaf993bd6beec20c3a
lucasgaraglia <garaglialucas@gmail.com>
2024-06-05 21:33:54 -0300

    solo nombre img

--- MapAware/src/main/java/com/mapaware/service/AuthService.java
+++ MapAware/src/main/java/com/mapaware/service/AuthService.java
@@ -41,7 +41,7 @@ public class AuthService {
                 .lastname(request.getLastname())
                 .name(request.getName())
                 .birthdate(request.getBirthdate())
-                .profileImage("/profileImages/default-profile.png")
+                .profileImage("default-profile.png")
                 .build();
 
         userRepository.save(user);
