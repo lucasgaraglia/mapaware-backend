3041f20ebf5fa163caab75ae2c7ea3a42086cd19
lucasgaraglia <garaglialucas@gmail.com>
2024-06-08 20:26:41 -0300

    variables de entorno & bdd ddl update, produccion

--- MapAware/src/main/java/com/mapaware/Jwt/JwtService.java
+++ MapAware/src/main/java/com/mapaware/Jwt/JwtService.java
@@ -5,6 +5,7 @@ import com.mapaware.service.UserService;
 import io.jsonwebtoken.*;
 import io.jsonwebtoken.security.SignatureException;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
@@ -22,8 +23,7 @@ public class JwtService {
     private final UserService userService;
 
 
-//    @Value("${jwt.secret}")
-    private final String secretKey = "pMe49rDiYgX+WaBeQSDNqikgekcqYZAQB1Vi3iIWDLnIoHVqyP1aBAt2rmh56aNXfNQAvsK4zdW4QGVBOY0sbOklnzjaXnuHgIBH8BdiHRxLw248+Lg/xlw7QWLM0VXqkWYmWv3mwd9+c6a4H00Zp62Q8ko29LadP10KyYo6/RVU6EMTDK8hKz06YIG01jN0cYhBdIA9PW1IWevQBAh17sQr/tfeCD/fsO3xposfu3I8B7QkQyoIdWWeSYozHROjHXSk6FtW4Ci/2t7HFLBh4Y8ez0fsSWrkdZ4t9Zge2sj5RWpjX8KDcq0XI9s0AoZeaahRW1ygE4jO8CRApR1Rbg==";
+    private final String secretKey = System.getenv("MW_JWT_SECRET");;
 
 //    @Value("${jwt.expiration}")
     private final int expirationTime = 86400000;
